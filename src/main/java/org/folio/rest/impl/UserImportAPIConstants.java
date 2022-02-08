4b0d9176f81ee322127dfc04b1a969ecbdd7d4fa
Adam Dickmeiss <adam@indexdata.dk>
2022-02-08 09:44:38 +0100

    Fix code smell

--- src/main/java/org/folio/rest/impl/UserImportAPIConstants.java
+++ src/main/java/org/folio/rest/impl/UserImportAPIConstants.java
@@ -35,9 +35,8 @@ public class UserImportAPIConstants {
   public static final String HTTP_HEADER_VALUE_TEXT_PLAIN = "text/plain";
   public static final String HTTP_HEADER_ACCEPT = "Accept";
 
-  public static final String PUT_CUSTOM_FIELDS_ENDPOINT = "/custom-fields";
+  public static final String CUSTOM_FIELDS_ENDPOINT = "/custom-fields";
   public static final String LIMIT_ALL = "?limit=" + Integer.MAX_VALUE;
-  public static final String GET_CUSTOM_FIELDS_ENDPOINT = "/custom-fields" + LIMIT_ALL;
   public static final String GET_MODULE_ID_ENDPOINT = "/_/proxy/tenants/%s/modules?provide=%s";
   public static final String USERS_INTERFACE_NAME = "users";
 
